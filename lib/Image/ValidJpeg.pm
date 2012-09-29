b983b5972c0f901c004e6b362a582cbe9df13bb4
Steve Sanbeg <ssanbeg@users.sourceforge.net>
2012-09-29 18:09:11 -0400

    rm autoloader

--- lib/Image/ValidJpeg.pm
+++ lib/Image/ValidJpeg.pm
@@ -6,7 +6,6 @@ use warnings;
 use Carp;
 
 require Exporter;
-use AutoLoader;
 
 our @ISA = qw(Exporter);
 
@@ -18,7 +17,7 @@ our @ISA = qw(Exporter);
 # If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
 # will save memory.
 our %EXPORT_TAGS = ( 
-    'all' => [ qw(
+    all => [ qw(
 check_tail
 check_jpeg
 check_all
@@ -28,7 +27,7 @@ SHORT
 EXTRA
 ) ], 
 
-'constants' => [ qw(
+constants => [ qw(
 GOOD
 BAD
 SHORT
@@ -39,45 +38,14 @@ EXTRA
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our @EXPORT = qw(
-	
-);
+our @EXPORT;
 
-our $VERSION = '1.001';
-
-sub AUTOLOAD {
-    # This AUTOLOAD is used to 'autoload' constants from the constant()
-    # XS function.
-
-    my $constname;
-    our $AUTOLOAD;
-    ($constname = $AUTOLOAD) =~ s/.*:://;
-    croak "&Image::ValidJpeg::constant not defined" if $constname eq 'constant';
-    my ($error, $val) = constant($constname);
-    if ($error) { croak $error; }
-    {
-	no strict 'refs';
-	# Fixed between 5.005_53 and 5.005_61
-#XXX	if ($] >= 5.00561) {
-#XXX	    *$AUTOLOAD = sub () { $val };
-#XXX	}
-#XXX	else {
-	    *$AUTOLOAD = sub { $val };
-#XXX	}
-    }
-    goto &$AUTOLOAD;
-}
+our $VERSION = '1.001001';
 
 require XSLoader;
 XSLoader::load('Image::ValidJpeg', $VERSION);
 
-# Preloaded methods go here.
-
-# Autoload methods go after =cut, and are processed by the autosplit program.
-
-1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
